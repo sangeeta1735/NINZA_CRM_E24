a81eb098fdfb201ed9e25946e22f7482b8ca7f68
Sangeeta <Sangeeta@Sangeeta>
2025-09-16 17:46:34 +0530

    changes from 2nd user

--- src/test/java/pack1/Test2.java
+++ src/test/java/pack1/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("CHANGES");
-		System.out.println("Test2");
+		System.out.println("change Test2");
 	}
 
 }
