63db249204bd9452a5ac0387466ad999b2153ef9
Sangeeta <Sangeeta@Sangeeta>
2025-09-16 17:17:30 +0530

    Test2.java

--- src/test/java/pack1/Test2.java
+++ src/test/java/pack1/Test2.java
@@ -1,10 +1,11 @@
-package pack2;
+package pack1;
 
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test2 Hi this is added by Neetu ");
+
+		System.out.println("Test2");
 	}
 
 }
